7748b2597dce60e1d04b70c70e2b841b54a14714
mmmeyers <marsi286@gmail.com>
2017-02-25 16:13:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 const issues = [
   {
     "body": "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team.",
@@ -9000,3 +9003,40 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+const issuesWithUpdatedApiUrl = issues.map(function(issue) {
+  return Object.assign({}, issue, {
+    url: issue.url.replace('api.github.com', 'api-v2.github.com')
+  })
+})
+
+const commentCountAcrossIssues = issues.map(issue => issue.comments_count)
+  .reduce((total, count) => total + count, 0);
+
+const openIssues = issues.reduce(function(openIssues, issue) {
+    if (issue.state === 'open') {
+      return [...openIssues, issue];
+    }
+
+    return openIssues;
+  }, []);
+
+const nonAutomaticIssues = issues.reduce(function(totalIssues, issue) {
+  const isAutomaticIssue = issue.body.includes('automatically created by learn.co');
+
+  if (!isAutomaticIssue) {
+    totalIssues.push(issue);
+  }
+
+  return totalIssues;
+}, []);
+
+const $tbody = document.getElementById('results');
+$tbody.innerHTML = nonAutomaticIssues
+  .map(issue => `<tr>
+    <td>${issue.body}</td>
+    <td>${issue.created_at}</td>
+    <td>${issue.state}</td>
+    </tr>`
+  )
+  .join('');
