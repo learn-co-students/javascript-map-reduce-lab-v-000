97743802eeaa1da8ac6fb399347b41142abdfb46
vanitanaidu <vanita112003@gmail.com>
2017-07-20 20:56:47 -0400

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,13 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+var issuesWithUpdatedApiUrl = issues.map((issue) =>
+   Object.assign({}, issue, {
+    url: issue.replace('api.github.com', 'api-v2.github.com')
+  }))
+
+// const issuesWithUpdatedApiUrl = issues.map(issue =>
+//   Object.assign({}, issue, {
+//     url: issue.url.replace('api.github.com', 'api-v2.github.com')
+//   }));
