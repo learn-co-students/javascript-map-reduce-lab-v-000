6f60334cb78d3ed9d2b258830e386eda99c09a31
Bruce Molina <bruce.molina.81@gmail.com>
2017-08-20 15:52:47 +0000

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,9 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+
+// var issuesWithUpdatedApiUrl = issues.map(
+//   (issue) => Object.assign( {}, issue, issue.url.replace(/api.github.com/,'api-v2.github.com')
+//  )
+// )
